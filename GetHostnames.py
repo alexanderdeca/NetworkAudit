a1661968289f176251b20305444cb335dd2ff1e3
Alexander Deca <alexander@deca-consulting.be>
2023-07-10 14:55:32 +0100

    Update files

--- GetHostnames.py
+++ GetHostnames.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime
 logger = logging.getLogger(__name__)
 
 # Define the CSV file path containing the device details
-csv_file = 'hosts_v2.csv'
+csv_file = 'hosts_telio.csv'
 
 # Create a list to store the devices
 devices = []
